29edade91602d31d514e6c819fbfa38b6c1f16c1
Kunanon <gent_playbox@hotmail.com>
2013-11-17 05:46:59 +0700

    create catalog activity and cart activity

--- MVCMobilePOS/gen/com/example/mvcmobilepos/R.java
+++ MVCMobilePOS/gen/com/example/mvcmobilepos/R.java
@@ -26,25 +26,27 @@ public final class R {
     public static final class id {
         public static final int ColItemID=0x7f080015;
         public static final int ColName=0x7f080016;
-        public static final int ColQuantity=0x7f080017;
+        public static final int ColQuantity=0x7f08001c;
         public static final int LinearLayout1=0x7f080010;
+        public static final int RelativeLayout1=0x7f080013;
         public static final int TotalPrice=0x7f080002;
-        public static final int action_settings=0x7f08001c;
+        public static final int action_settings=0x7f08001e;
         public static final int btnCancel=0x7f080012;
         public static final int btnSave=0x7f080011;
-        public static final int button1=0x7f080013;
-        public static final int button2=0x7f080019;
-        public static final int button3=0x7f080014;
-        public static final int button4=0x7f08001a;
-        public static final int button6=0x7f08001b;
+        public static final int button1=0x7f080019;
+        public static final int button2=0x7f080018;
+        public static final int button3=0x7f08001b;
+        public static final int button4=0x7f08001d;
+        public static final int button6=0x7f08001a;
         public static final int buttonScan=0x7f080006;
-        public static final int listView1=0x7f080018;
+        public static final int listView1=0x7f080017;
         public static final int tableLayout1=0x7f080000;
         public static final int tableRow1=0x7f080001;
         public static final int tableRow2=0x7f080003;
         public static final int tableRow3=0x7f080007;
         public static final int tableRow4=0x7f08000a;
         public static final int tableRow5=0x7f08000d;
+        public static final int textView1=0x7f080014;
         public static final int textView2=0x7f080004;
         public static final int textView3=0x7f080008;
         public static final int textView4=0x7f08000b;
@@ -56,17 +58,19 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_add=0x7f030000;
-        public static final int activity_catalog=0x7f030001;
-        public static final int activity_history=0x7f030002;
-        public static final int activity_inventory=0x7f030003;
-        public static final int activity_setting=0x7f030004;
+        public static final int activity_cart=0x7f030001;
+        public static final int activity_catalog=0x7f030002;
+        public static final int activity_history=0x7f030003;
+        public static final int activity_inventory=0x7f030004;
+        public static final int activity_setting=0x7f030005;
     }
     public static final class menu {
         public static final int add_screen=0x7f070000;
-        public static final int catalog_screen=0x7f070001;
-        public static final int history_screen=0x7f070002;
-        public static final int inventory=0x7f070003;
-        public static final int setting_screen=0x7f070004;
+        public static final int cart_screen=0x7f070001;
+        public static final int catalog_screen=0x7f070002;
+        public static final int history_screen=0x7f070003;
+        public static final int inventory=0x7f070004;
+        public static final int setting_screen=0x7f070005;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -79,6 +83,7 @@ public final class R {
         public static final int button_setup=0x7f050008;
         public static final int hello_world=0x7f050002;
         public static final int title_activity_add_screen=0x7f05000a;
+        public static final int title_activity_cart_screen=0x7f05000d;
         public static final int title_activity_catalog_screen=0x7f050009;
         public static final int title_activity_history_screen=0x7f05000b;
         public static final int title_activity_setting_screen=0x7f05000c;
--- MVCMobilePOS/src/com/example/mvcmobilepos/CatalogScreen.java
+++ MVCMobilePOS/src/com/example/mvcmobilepos/CatalogScreen.java
@@ -2,7 +2,10 @@ package com.example.mvcmobilepos;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class CatalogScreen extends Activity {
 
@@ -10,7 +13,39 @@ public class CatalogScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_catalog);
+		
+		//back to inventory button
+		final Button btn_Invntory = (Button) findViewById(R.id.button2);
+		// Perform action on click
+		btn_Invntory.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				Intent newActivity = new Intent(CatalogScreen.this,
+						InventoryScreen.class);
+				
+				startActivity(newActivity);
+				finish();
+
+			}
+		});
+		
+		//go to cart button
+		final Button btn_Cart = (Button) findViewById(R.id.button6);
+		// Perform action on click
+		btn_Cart.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+				Intent newActivity = new Intent(CatalogScreen.this,
+						CartScreen.class);
+				
+				startActivity(newActivity);
+				finish();
+
+			}
+		});
 	}
+
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
